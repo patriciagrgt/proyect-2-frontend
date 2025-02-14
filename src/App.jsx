4fbee177233bd98ee59d0c4db1bfed4f7c5ce830
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-02-14 17:44:53 +0000

    created structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Feed from "./pages/Feed";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
 
 function App() {
 
   return (
     <>
-      hola
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+      </Routes>
     </>
   )
 }
